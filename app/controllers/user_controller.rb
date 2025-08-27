12e770671d038c1c01500334eb8a02b0ca4758bb
hosseinyn <hosseinyadegarnia1391@gmail.com>
2025-08-27 21:28:03 +0330

    Add signup feature && Add some validations && Fix some problems

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,10 +16,28 @@ class UserController < ApplicationController
     end
   end
 
+  def signup
+    @user = User.new
+  end
+
+  def register
+    @user = User.new(user_params)
+    
+    if @user.save 
+      flash[:notice] = "Signed up successfully."
+      redirect_to "/login"
+    else 
+      flash[:alert] = @user.errors.full_messages.to_sentence
+      render :signup
+
+    end
+
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password , :password_confirmation)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get "links/add"
   get "login" , to: "user#signin"
   post "login" , to: "user#login"
+
+  get "signup" , to: "user#signup"
+  post "signup" , to: "user#register"
   root "index#index"
 end
